63cd0351cd703f97c7369e435cd800f59a4184eb
Fastdrecad <andrijas.micun@gmail.com>
2023-12-26 10:23:25 +0100

    Create pages and routes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import HomePage from "pages/HomePage";
+import LoginPage from "pages/LoginPage";
+import ProfilePage from "pages/ProfilePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
